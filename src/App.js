849a90fb158ec6b98dfaeaece90374c4df16684e
enicio <jessandro42@gmail.com>
2021-05-28 14:17:24 -0300

    Adicionado o modal

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Header from './components/header'
 import Details from './components/details';
 import Content from './components/content';
 
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <Header/>
-      <div className="main">
+      <div className="main" id="main">
         <div className="delails_left">
           <Details />
           <img className="delails_left_woman" src='./assets/image/image.png' alt="woman writing" />
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,29 @@
 import React from 'react';
 // import logo from '../assets/header/logo.svg'
 
+import ModalF from './modal';
+
 import './header.css'
 
 function Header() {
+  const [modalIsOpen,setIsOpenHeader] = React.useState(false);
+
+ function openModalHeader(isOpen) {
+    setIsOpenHeader(isOpen);
+  }
+
   return(
+    <>
     <header className="header">
       <img className="header__logo" src='assets/header/logo.svg' alt="logo"/>
       <nav className="header__menu">
         <p>How it works </p>
         <p>About Us</p>
       </nav>
-      <button className="header__button">Get Started</button>
+      <button onClick={ () => openModalHeader(true)} className="header__button">Get Started</button>
     </header>
+    {(modalIsOpen)?<ModalF openModal={openModalHeader} />: ''}
+    </>
   );
 }
 
